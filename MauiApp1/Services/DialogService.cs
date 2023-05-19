b4e0702819566c986a45c6ed39cab8b27842987e
lochi <72388533+lochidev@users.noreply.github.com>
2023-05-19 19:32:40 +0530

    Add project files.

---
+++ MauiApp1/Services/DialogService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    internal class DialogService : IDialogService
+    {
+        public async Task DisplayConfirmAsync(string title, string message)
+        {
+            await Application.Current.MainPage.DisplayAlert(title, message, "Ok");
+        }
+    }
+}
---
+++ MauiApp1/Services/IDialogService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    internal interface IDialogService
+    {
+        Task DisplayConfirmAsync(string title, string message);
+    }
+}
