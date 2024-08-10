1fa83ed1518aefd387603ca0571870f307cb3a76
lochi <72388533+lochidev@users.noreply.github.com>
2024-08-10 17:31:10 +0530

    export compatibility fix

--- src/Platforms/Android/Services/ExpanderAccessibilityService.cs
+++ src/Platforms/Android/Services/ExpanderAccessibilityService.cs
@@ -427,8 +427,6 @@ public class ExpanderAccessibilityservice : AccessibilityService, Android.Views.
                 rowContainer.RemoveAllViewsInLayout();
                 skipNextFormEvent = true;
             };
-            windowManager = GetSystemService(WindowService).JavaCast<IWindowManager>();
-            windowManager.AddView(floatView, layoutParams);
             floatView.SetOnTouchListener(this);
             rowContainer = floatView.FindViewById<LinearLayout>(Microsoft.Maui.Resource.Id.rowContainer);
         }
